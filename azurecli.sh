683bc2ffdf59c6cfa448662ec6db46736d06afdd
aliciathc <aliciathc@gmail.com>
2015-05-13 15:20:22 +0800

    fix bug at line33 & improve installation status view

--- azurecli.sh
+++ azurecli.sh
@@ -2,36 +2,50 @@
 
 set -e
 
-echo "Update packges..."
+echo "##############################################"
+echo "Update packages..."
+echo "##############################################"
 sudo apt-get update
 sleep 3
 
+echo "##############################################"
 echo "Installing nodejs-legacy.."
+echo "##############################################"
 sudo apt-get install nodejs-legacy -y
 sleep 3
 echo "Done"
 
+echo "##############################################"
 echo "Installing npm..."
+echo "##############################################"
 sudo apt-get install npm -y
 sleep 3
 echo "Done"
 
+echo "##############################################"
 echo "Installing curl..."
+echo "##############################################"
 sudo apt-get install curl -y
 sleep 3
 echo "Done"
 
-echo"Downloading from https://deb.nodesource.com/setup.."
+echo "##############################################"
+echo "Downloading from https://deb.nodesource.com/setup.."
+echo "##############################################"
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sleep 3
 echo "Done"
 
+echo "##############################################"
 echo "Installing nodejs.."
+echo "##############################################"
 sudo apt-get install -y nodejs
 sleep 3
 echo "Done"
 
+echo "##############################################"
 echo "Installing azure-cli..."
+echo "##############################################"
 sudo npm install -g azure-cli 
 sleep 3
 
